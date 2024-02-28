15aeec752ec6b701b4c28dc77e12a2f7e4e40b7a
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-02-28 12:06:18 +0100

    feat: server listening

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
-"use strict";
-console.log('Hello API');
+import express from 'express';
+import bodyParser from 'body-parser';
+import http from 'http';
+const app = express();
+app.use(bodyParser.json());
+const server = http.createServer(app);
+const PORT = 3000;
+app.get('/', (req, res) => {
+    res.json('Hello world');
+});
+server.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}/`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello API')
+import express from 'express';
+import bodyParser from 'body-parser';
+import http from 'http';
+
+const app: express.Application = express();
+
+app.use(bodyParser.json());
+
+const server: http.Server = http.createServer(app);
+
+const PORT = 3000;
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.json('Hello world');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}/`);
+});
