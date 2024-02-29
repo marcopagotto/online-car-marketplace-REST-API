aa7fa92eb113c68a55e1d18f8ceb33db2dfdb8cc
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-02-29 14:55:55 +0100

    feat(login): login router

--- dist/routes/register.js
+++ dist/routes/register.js
@@ -1,4 +1,5 @@
-import { registerUser } from '../controllers/authentication.js';
+import { registerUser, loginUser } from '../controllers/authentication.js';
 export default (router) => {
     router.post('/auth/register', registerUser);
+    router.post('/auth/login', loginUser);
 };
--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import { registerUser } from '../controllers/authentication.js';
+import { registerUser, loginUser } from '../controllers/authentication.js';
 
 export default (router: express.Router) => {
   router.post('/auth/register', registerUser);
+  router.post('/auth/login', loginUser);
 };
