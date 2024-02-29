841ebe6e456165f9d0d9a9e0c56a44b5f9657e38
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-02-29 14:52:35 +0100

    feat(login): add login controller

--- dist/controllers/authentication.js
+++ dist/controllers/authentication.js
@@ -26,3 +26,31 @@ export const registerUser = async (req, res) => {
         return res.sendStatus(400);
     }
 };
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.sendStatus(400);
+        }
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+        if (!user) {
+            return res.sendStatus(400);
+        }
+        const expectedPassword = authentication(user.authentication?.salt, password);
+        if (user.authentication?.password !== expectedPassword) {
+            return res.sendStatus(403);
+        }
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+        res.cookie('auth-login', user.authentication.sessionToken, {
+            domain: 'localhost',
+            path: '/',
+            maxAge: 100000,
+        });
+        res.status(200).json(user).end();
+    }
+    catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+};
--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { random, authentication } from '../library/library.js';
-import { createUser, getUserByEmail, getUsers } from '../db/users.js';
+import { createUser, getUserByEmail } from '../db/users.js';
 
 export const registerUser = async (
   req: express.Request,
@@ -31,6 +31,53 @@ export const registerUser = async (
     });
 
     return res.status(201).json(newUser).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const loginUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email).select(
+      '+authentication.salt +authentication.password'
+    );
+
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    const expectedPassword = authentication(
+      user.authentication?.salt!,
+      password
+    );
+
+    if (user.authentication?.password !== expectedPassword) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    res.cookie('auth-login', user.authentication.sessionToken, {
+      domain: 'localhost',
+      path: '/',
+      maxAge: 100000,
+    });
+
+    res.status(200).json(user).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
