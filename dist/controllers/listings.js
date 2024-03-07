bae68c47215544f2c6d706410392a6648db47b03
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-03-07 07:12:41 +0100

    feat(list): posting listing now accepts carId

--- dist/controllers/listings.js
+++ dist/controllers/listings.js
@@ -1,17 +1,23 @@
 import { createListing } from '../db/listings.js';
-import { getUserById } from '../db/users.js';
+import { getUserById, getCarOwnerByCarId } from '../db/users.js';
 export const newListing = async (req, res) => {
     try {
-        const { car, owner, price } = req.body;
-        if (!car || !owner || !price) {
+        const { carId, owner, price } = req.body;
+        if (!carId || !owner || !price) {
             return res.sendStatus(400);
         }
         const user = await getUserById(owner.toString());
-        console.log(user);
         if (!user) {
             return res.sendStatus(400);
         }
+        const expectedUser = await getCarOwnerByCarId(carId.toString());
+        if (JSON.stringify(user) !== JSON.stringify(expectedUser)) {
+            return res.sendStatus(400);
+        }
+        const car = user.cars.find((car) => car._id.toString() === carId);
+        console.log(car);
         const listing = await createListing(car, owner, price);
+        console.log(listing);
         return res.status(200).json(listing).end();
     }
     catch (error) {
--- src/controllers/listings.ts
+++ src/controllers/listings.ts
@@ -3,32 +3,41 @@ import mongoose from 'mongoose';
 
 import { createListing } from '../db/listings.js';
 import { CarSchema } from '../db/users.js';
-import { getUserById } from '../db/users.js';
+import { getUserById, getCarOwnerByCarId } from '../db/users.js';
 
 export const newListing = async (
   req: express.Request,
   res: express.Response
 ) => {
   try {
-    const { car, owner, price } = req.body as {
-      car: typeof CarSchema;
+    const { carId, owner, price } = req.body as {
+      carId: mongoose.Schema.Types.ObjectId;
       owner: mongoose.Schema.Types.ObjectId;
       price: number;
     };
 
-    if (!car || !owner || !price) {
+    if (!carId || !owner || !price) {
       return res.sendStatus(400);
     }
 
     const user = await getUserById(owner.toString());
 
-    console.log(user);
-
     if (!user) {
       return res.sendStatus(400);
     }
 
+    const expectedUser = await getCarOwnerByCarId(carId.toString());
+
+    if (JSON.stringify(user) !== JSON.stringify(expectedUser)) {
+      return res.sendStatus(400);
+    }
+
+    const car = user.cars.find((car) => car._id.toString() === carId);
+
+    console.log(car);
     const listing = await createListing(car, owner, price);
+
+    console.log(listing);
 
     return res.status(200).json(listing).end();
   } catch (error) {
