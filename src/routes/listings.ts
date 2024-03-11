524b6da845b9dc193fe888d1df0dbac65bd5b265
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-03-11 19:16:56 +0100

    chore: removed unnecessary imports

--- src/routes/listings.ts
+++ src/routes/listings.ts
@@ -2,7 +2,6 @@ import express from 'express';
 
 import { newListing, findListings } from '../controllers/listings.js';
 import { isAuthenticated, isCarOwner } from '../middlewares/middlewares.js';
-import { getListings } from '../db/listings.js';
 
 export default (router: express.Router) => {
   router.post('/listings', isAuthenticated, isCarOwner, newListing);
