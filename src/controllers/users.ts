57062283d787e778c592730310a56a2548e26dbb
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-03-18 16:31:34 +0100

    fix(users): invalid car id

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -90,9 +90,13 @@ export const getOwner = async (req: express.Request, res: express.Response) => {
     if (!id) {
       return res.sendStatus(400);
     }
-    console.log(id);
+
     const owner = await getCarOwnerByCarId(id);
-    console.log(owner);
+
+    if (!owner) {
+      return res.sendStatus(400);
+    }
+    
     return res.status(200).json(owner).end();
   } catch (error) {
     console.log(error);
